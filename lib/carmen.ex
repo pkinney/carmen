543b880c269c2e1537fc9e449d2c22dea7f617b5
Jerel Unruh <jerel.unruh@toyotaconnected.com>
2017-09-22 15:56:45 -0500

    Remove redis for now

--- lib/carmen.ex
+++ lib/carmen.ex
@@ -5,7 +5,6 @@ defmodule Carmen do
     import Supervisor.Spec, warn: false
 
     children = [
-      supervisor(Carmen.Redis.Pool, [[host: "localhost", port: 6379]]),
       supervisor(Carmen.Zone.Pool, [[{-180, 180, 0.001}, {-90, 90, 0.001}]]),
       supervisor(Carmen.Object.Supervisor, [])
     ]
