28a24e9b5cb76e394327174fe1f4b08bbb2f866c
Jerel Unruh <jerel.unruh@toyotaconnected.com>
2018-08-07 11:17:24 -0500

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Carmen.Mixfile do
   def project do
     [
       app: :carmen,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       description: description(),
       package: package(),
