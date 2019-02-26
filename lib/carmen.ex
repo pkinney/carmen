8b5671e2618ec9a4c891cea58fb2a16e874949b0
Powell Kinney <pkinney@gmail.com>
2019-02-26 12:27:37 -0600

    Revert "Make the cell size configurable at runtime"

--- lib/carmen.ex
+++ lib/carmen.ex
@@ -3,17 +3,11 @@ defmodule Carmen do
   use Application
 
   @interface Application.get_env(:carmen, :interface, Carmen.InterfaceExample)
-  @default_opts %{pool_opts: [{-180, 180, 0.001}, {-90, 90, 0.001}]}
 
-  def start(type, opts \\ @default_opts)
-  def start(type, opts) when not is_map(opts), do: start(type, @default_opts)
-
-  def start(_type, opts) do
-    opts = Map.merge(@default_opts, opts)
-
+  def start(_type, _args) do
     children = [
       {Registry, [keys: :unique, name: Carmen.Registry]},
-      {Carmen.Zone.Pool, opts.pool_opts},
+      {Carmen.Zone.Pool, [{-180, 180, 0.001}, {-90, 90, 0.001}]},
       {Carmen.Object.Supervisor, nil},
       Supervisor.child_spec({Task, &load_zones/0}, %{restart: :transient})
     ]
